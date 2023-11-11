b289a05d32732e9c50b6717890b4605e3e754ab0
opensvn <justleefeng@gmail.com>
2023-11-11 21:54:07 +0800

    0438.Find All Anagrams in a String

---
+++ 0438.Find All Anagrams in a String/solution.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=golang
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+
+// @lc code=start
+func findAnagrams(s string, p string) []int {
+	need, window := make(map[byte]int), make(map[byte]int)
+	for i := range p {
+		need[p[i]]++
+	}
+	var left, right, valid int
+	var res []int
+	for right < len(s) {
+		c := s[right]
+		right++
+		if _, ok := need[c]; ok {
+			window[c]++
+			if window[c] == need[c] {
+				valid++
+			}
+		}
+
+		for valid == len(need) {
+			if right - left == len(p) {
+				res = append(res, left)
+			}
+			d := s[left]
+			left++
+			if _, ok := need[d]; ok {
+				if window[d] == need[d] {
+					valid--
+				}
+				window[d]--
+			}
+		}
+	}
+	return res
+}
+// @lc code=end
