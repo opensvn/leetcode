6fa7c7e3d1b69d6486dd783fedee7719eb93ddd9
opensvn <justleefeng@gmail.com>
2023-11-13 21:08:22 +0800

    0092.Reverse Linked List II

---
+++ 0092.Reverse Linked List II/solution.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=golang
+ *
+ * [92] 反转链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if left == 1 {
+		return reverseN(head, right)
+	}
+
+	head.Next = reverseBetween(head.Next, left-1, right-1)
+	return head
+}
+
+var successor *ListNode
+func reverseN(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		successor = head.Next
+		return head
+	}
+
+	newHead := reverseN(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = successor
+	return newHead
+}
+// @lc code=end
