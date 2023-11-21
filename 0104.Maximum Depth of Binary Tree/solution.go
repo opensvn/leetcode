7f29b10e46c0c76af7cc0c7d464d4959eb2ba7be
opensvn <justleefeng@gmail.com>
2023-11-21 12:38:50 +0800

    0104.Maximum Depth of Binary Tree

---
+++ 0104.Maximum Depth of Binary Tree/solution.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	return max(leftDepth, rightDepth) + 1
+}
+// @lc code=end
