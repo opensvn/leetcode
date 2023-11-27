9311b12e31778130a1dd1ad94c8e2247282f011a
opensvn <justleefeng@gmail.com>
2023-11-27 21:23:14 +0800

    0538.Convert BST to Greater Tree

---
+++ 0538.Convert BST to Greater Tree/solution.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=golang
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+	var sum int
+	var traverse func(*TreeNode)
+	traverse = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		traverse(root.Right)
+		sum += root.Val
+		root.Val = sum
+		traverse(root.Left)
+	}
+	traverse(root)
+	return root
+}
+// @lc code=end
